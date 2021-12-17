43f27c7ec4166122de7d1841eb9565362e4c1c98
Ahmad Ghorbani <56560408+Ahmad-Ghorbani@users.noreply.github.com>
2021-12-17 17:22:33 +0330

    update profile

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./Navigation.css";
 import profileImage from "./icon-profile-8.jpg";
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
 const Navigation = ({ onRouteChange, isSignedin }) => {
   if (isSignedin) {
     return (
-      <nav className="Navigation">
-        {/* <p className="letin" onClick={() => onRouteChange("signin")}>
-          Sign out
-        </p> */}
-        <img
-          className="signout"
-          onClick={() => onRouteChange("signin")}
-          src={profileImage}
-          alt="profile "
-        />
-      </nav>
+      <div>
+        <nav className="Navigation">
+          {/* <p className="letin" onClick={() => onRouteChange("signin")}>
+            Sign out
+          </p> */}
+          <img
+            className="signout dropbtn"
+            onClick={() => myFunction()}
+            // onRouteChange("signin")
+            src={profileImage}
+            alt="profile "
+          />
+          <div id="myDropdown" className="dropdown-content">
+            <a onClick={() => onRouteChange("signin")}>Sign out</a>
+            <a>Your profile</a>
+          </div>
+        </nav>
+      </div>
     );
   } else {
     return (isSignedin = false);
